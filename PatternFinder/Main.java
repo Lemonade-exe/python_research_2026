e9240ef108b3c995a3ed6d668c16266260f04bc2
RoboticsAZ <alexzhuo2009@gmail.com>
2026-04-08 00:10:16 -0400

    Fix: closed the objects

--- PatternFinder/Main.java
+++ PatternFinder/Main.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File spreadsheet = new File("file.csv"); // replace the text within the "" with the file you want to obtain the data from
+        File spreadsheet = new File("RFScope(0-100).csv"); // replace the text within the "" with the file you want to obtain the data from
         try {
             Scanner reader = new Scanner(spreadsheet);
-            FileWriter fw = new FileWriter("file.csv", true); // replace the text within the "" with the file that you want the found values be stored in
+            FileWriter fw = new FileWriter("RFResult.csv", true); // replace the text within the "" with the file that you want the found values be stored in
             PrintWriter pw = new PrintWriter(fw);
             reader.nextLine();
 
@@ -20,6 +20,9 @@ public class Main {
                 }
                 row ++;
             }
+            pw.flush();
+            pw.close();
+            fw.close();
             reader.close();
         }
         catch (IOException e) {
