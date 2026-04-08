06bbc4f86bd5d265c7af65e2404c4e01a93b4b1f
RoboticsAZ <alexzhuo2009@gmail.com>
2026-04-07 23:51:49 -0400

    update: fixed the pattern finder code

--- PatternFinder/Main.java
+++ PatternFinder/Main.java
@@ -3,22 +3,22 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File spreadsheet = new File("KNNBigScope.csv");
+        File spreadsheet = new File("file.csv"); // replace the text within the "" with the file you want to obtain the data from
         try {
             Scanner reader = new Scanner(spreadsheet);
-            FileWriter fw = new FileWriter("DTResult.csv", true);
+            FileWriter fw = new FileWriter("file.csv", true); // replace the text within the "" with the file that you want the found values be stored in
             PrintWriter pw = new PrintWriter(fw);
             reader.nextLine();
 
+            int row = 1;
             while (reader.hasNext()) {
-                int row = 1;
                 String line = reader.nextLine();
                 String[] info = line.split(",");
                 System.out.println(line);
-                if(Double.parseDouble(info[info.length-1]) > 0.52) {
+                if(Double.parseDouble(info[info.length-1]) >= 0.52) {
                     pw.println(row + "," + line);
                 }
-                row++;
+                row ++;
             }
             reader.close();
         }
